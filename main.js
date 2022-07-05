43633ca34839f02efbdf8abf4a19e2b1dbe9fff9
EC2 Default User <ec2-user@ip-172-31-36-170.us-west-2.compute.internal>
2022-07-05 00:19:54 +0000

    インデントと改行の修正

--- main.js
+++ main.js
@@ -1,100 +1,69 @@
- 
- 
- 
-      $(document).ready(function(){
-       
-       
-     
-     let clock = document.getElementById("clock");
-     let start = document.getElementById("start");
-     let stop = document.getElementById("stop");
-     let reset = document.getElementById("reset");
-     
-     stop.disabled = true;
-     reset.disabled = true;
-     
-     
-    let ThouSec = 0;
-    let HundSec = 0;
-    let TenSec = 0;
-    let OneSec = 0;
-    
-    let timer;
-    
-    
 
-     
-     
-  function countup(){
-        
-      //カウントアップ
-        OneSec++;
-        
-        if(OneSec / 10 == 1){
-            TenSec++;
-            OneSec = 0;
-            
-            if(TenSec / 60 == 1){
-            HundSec++;
-            TenSec = 0;
-            
-             if(HundSec / 60 == 1){
-            ThouSec++;
-            HundSec = 0;
-            
-        }
+ let clock = document.getElementById("clock");
+ let start = document.getElementById("start");
+ let stop = document.getElementById("stop");
+ let reset = document.getElementById("reset");
  
-        }
-        
-        }
-         
-             
-        clock.innerHTML=ThouSec + ':' +  HundSec  + ':' + TenSec + ":" + OneSec;
-  
-            
-        }
+ stop.disabled = true;
+ reset.disabled = true;
+
+ let ThouSec = 0;
+ let HundSec = 0;
+ let TenSec = 0;
+ let OneSec = 0;
  
-  
+ let timer;
  
- 
-   start.addEventListener("click",function(){
-    timer = setInterval(countup,100)
+ function countup(){
+
+    OneSec++;
     
-    //表示非表示
-    stop.disabled = false;
-    reset.disabled = false;
-    start.disabled = true;
-   });
- 
- 
+      if(OneSec / 10 == 1){
+      TenSec++;
+      OneSec = 0;
+      
+        if(TenSec / 60 == 1){
+        HundSec++;
+        TenSec = 0;
+        
+          if(HundSec / 60 == 1){
+          ThouSec++;
+          HundSec = 0;
+          
+          }
+          
+        }
+      
+      }
+    clock.innerHTML=ThouSec + ':' +  HundSec  + ':' + TenSec + ":" + OneSec;
+  
+ }
+
+ start.addEventListener("click",function(){
+   timer = setInterval(countup,100)
+   //表示非表示
+   stop.disabled = false;
+   reset.disabled = false;
+   start.disabled = true;
+ });
+
  stop.addEventListener("click",function(){
    clearInterval(timer);
-   
    //表示非表示
    stop.disabled = true;
    start.disabled = false;
-  
  });
- 
+
  reset.addEventListener("click",function(){
-  clearInterval(timer);
-  clock.innerHTML = "0:0:0:0";
-    
-     ThouSec = 0;
-     HundSec = 0;
-     TenSec = 0;
-     OneSec = 0;
-    
-  
-  
-  
+   clearInterval(timer);
+   clock.innerHTML = "0:0:0:0";
+   
+   ThouSec = 0;
+   HundSec = 0;
+   TenSec = 0;
+   OneSec = 0;
    //表示非表示
-    stop.disabled = true;
-    reset.disabled = true;
-    start.disabled = false;
-  
+   stop.disabled = true;
+   reset.disabled = true;
+   start.disabled = false;
  });
-  
-  
- 
-    });
